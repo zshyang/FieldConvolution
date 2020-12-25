af19a2d61906f66c0a5848fb1eec5b977b900806
zshyang <zshyang1106@gmail.com>
2020-12-25 15:51:49 -0800

    Add function index_points

--- code/model/layer/field_convolution.py
+++ code/model/layer/field_convolution.py
@@ -166,6 +166,25 @@ def farthest_point_sample(xyz, npoint):
     return centroids
 
 
+def index_points(points, idx):
+    """
+    Input:
+        points: input points data, [B, N, C]
+        idx: sample index data, [B, S]
+    Return:
+        new_points:, indexed points data, [B, S, C]
+    """
+    device = points.device
+    B = points.shape[0]
+    view_shape = list(idx.shape)
+    view_shape[1:] = [1] * (len(view_shape) - 1)
+    repeat_shape = list(idx.shape)
+    repeat_shape[0] = 1
+    batch_indices = torch.arange(B, dtype=torch.long).to(device).view(view_shape).repeat(repeat_shape)
+    new_points = points[batch_indices, idx, :]
+    return new_points
+
+
 def forward(batch: {str: torch.Tensor}):
 
     # Get the convolution center index. (B, C). C is the number of convolution center.
