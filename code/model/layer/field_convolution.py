f420ef412a4eb90ed2b17eaac5c7492c3abbfb95
zshyang <zshyang1106@gmail.com>
2020-12-25 15:58:23 -0800

    Reformat the function index_points.

--- code/model/layer/field_convolution.py
+++ code/model/layer/field_convolution.py
@@ -166,22 +166,30 @@ def farthest_point_sample(xyz, npoint):
     return centroids
 
 
-def index_points(points, idx):
+def index_points(points: torch.Tensor, idx: torch.Tensor) -> torch.Tensor:
+    """Group the points given the index.
+
+    Args:
+        points: The input points data. (B, N, C)
+        idx: The sample index data. (B, S)
+
+    Returns:
+        new_points: The indexed points data. (B, S, C)
     """
-    Input:
-        points: input points data, [B, N, C]
-        idx: sample index data, [B, S]
-    Return:
-        new_points:, indexed points data, [B, S, C]
-    """
+
     device = points.device
-    B = points.shape[0]
+    batch_size = points.shape[0]
+
     view_shape = list(idx.shape)
     view_shape[1:] = [1] * (len(view_shape) - 1)
+
     repeat_shape = list(idx.shape)
     repeat_shape[0] = 1
-    batch_indices = torch.arange(B, dtype=torch.long).to(device).view(view_shape).repeat(repeat_shape)
+
+    batch_indices = torch.arange(batch_size, dtype=torch.long).to(device).view(view_shape).repeat(repeat_shape)
+
     new_points = points[batch_indices, idx, :]
+
     return new_points
 
 
