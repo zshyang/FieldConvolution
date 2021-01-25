d4e9e2d8a053793ba20c037ede9c3787cf1480f5
George <zshyang1106@gmail.com>
2021-01-25 00:01:02 -0700

    update test.py

--- PointNetBaseline/code/test.py
+++ PointNetBaseline/code/test.py
@@ -7,7 +7,12 @@ import sys
 from options import update_options, options, reset_options
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
+    """parse the arguments.
+
+    Returns:
+        args: the arguments.
+    """
     parser = argparse.ArgumentParser(description="Test")
     parser.add_argument('--options', help='experiment options file name', required=False, type=str)
 
@@ -18,24 +23,17 @@ def parse_args():
     else:
         update_options(args.options)
 
-    # parser.add_argument('--batch-size', help='batch size', type=int)
-    # parser.add_argument('--shuffle', help='shuffle samples', default=False, action='store_true')
-    # parser.add_argument('--checkpoint', help='trained checkpoint file', type=str, required=True)
-    # parser.add_argument('--version', help='version of task (timestamp by default)', type=str)
-    # parser.add_argument('--name', help='subfolder name of this experiment', required=True, type=str)
-    # parser.add_argument('--gpus', help='number of GPUs to use', type=int)
-    #
-    # args = parser.parse_args()
-    #
     return args
 
 
 def main():
     args = parse_args()
-    print(args)
+    print(type(args))
     import pprint
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(options)
+
+    logger, writer = reset_options(options, args, phase="test")
 
 
 if __name__ == '__main__':
