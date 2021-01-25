acc77b8050ca6cd5206d1a87abdcb11f8aa932fb
George <zshyang1106@gmail.com>
2021-01-25 16:48:14 -0700

    update

--- PointNetBaseline/code/sh/test_without_da.sh
+++ PointNetBaseline/code/sh/test_without_da.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --options experiments/baseline_pointnet2_without_da.yml
+CUDA_VISIBLE_DEVICES=0 python test.py --options experiments/baseline_pointnet2_without_da.yml
