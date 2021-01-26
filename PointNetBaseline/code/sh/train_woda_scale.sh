0115ac76d3b8509c0d2ace8d723bbef51b1e7a18
George <zshyang1106@gmail.com>
2021-01-26 01:14:48 -0700

    add train_woda_scale.sh

--- PointNetBaseline/code/sh/train_woda_scale.sh
+++ PointNetBaseline/code/sh/train_woda_scale.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python train.py --options experiments/baseline_pointnet2_scale.yml
+CUDA_VISIBLE_DEVICES=0 python train.py --options experiments/baseline_pointnet2_woda_scale.yml
