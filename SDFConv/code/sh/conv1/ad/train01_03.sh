2b2a5fbfaa59cf82156f9d4c8b17d227de7c6677
George <zshyang1106@gmail.com>
2021-02-13 20:20:06 -0700

    add few yml

---
+++ SDFConv/code/sh/conv1/ad/train01_03.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 python train.py --options experiments/conv1/ad/fold01.yml
+CUDA_VISIBLE_DEVICES=0 python train.py --options experiments/conv1/ad/fold02.yml
+CUDA_VISIBLE_DEVICES=0 python train.py --options experiments/conv1/ad/fold03.yml
---
+++ SDFConv/code/sh/testbed/gpu0.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python train.py --options experiments/vgg/ad/explore/gpu0.yml
