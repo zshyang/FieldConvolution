c3a64db5f5f7f3d8c9917753f139f04577466eaf
George <zshyang1106@gmail.com>
2021-02-19 15:17:38 -0700

    update sh file

--- SDFConv/code/sh/sparse2/nl05_09.sh
+++ SDFConv/code/sh/sparse2/nl05_09.sh
@@ -1,5 +1,10 @@
-CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse1/nl/fold05.yml
-CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse1/nl/fold06.yml
-CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse1/nl/fold07.yml
-CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse1/nl/fold08.yml
-CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse1/nl/fold09.yml
+CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse2/nl/fold05.yml
+CUDA_VISIBLE_DEVICES=3 python test.py  --options experiments/sparse2/nl/fold05_t.yml
+CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse2/nl/fold06.yml
+CUDA_VISIBLE_DEVICES=3 python test.py  --options experiments/sparse2/nl/fold06_t.yml
+CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse2/nl/fold07.yml
+CUDA_VISIBLE_DEVICES=3 python test.py  --options experiments/sparse2/nl/fold07_t.yml
+CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse2/nl/fold08.yml
+CUDA_VISIBLE_DEVICES=3 python test.py  --options experiments/sparse2/nl/fold08_t.yml
+CUDA_VISIBLE_DEVICES=3 python train.py --options experiments/sparse2/nl/fold09.yml
+CUDA_VISIBLE_DEVICES=3 python test.py  --options experiments/sparse2/nl/fold09_t.yml
